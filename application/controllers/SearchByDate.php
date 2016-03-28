67321a0e78da2682ee8f285952b7a01fe027ff2d
ZhuYifan <zhuyf8899@gmail.com>
2016-03-28 16:53:26 +0800

    0.0.2.1 - change the brench to fit the new script

--- application/controllers/SearchByDate.php
+++ application/controllers/SearchByDate.php
@@ -16,9 +16,9 @@ class SearchByDate extends CI_Controller {
 		$this->load->view('error');
 	}
 
-	public function selectOneDate($date = ''){	//参数格式务必保证是yyyy-mm-dd.
+	public function selectOneDateEp($date = ''){	//参数格式务必保证是yyyy-mm-dd.
 		if(empty($date)){
-			$date = date('20y-m-d');
+			$date = date('Y-m-d');
 		}
 		$data['result'] =  array();
 		$data['errorFlag'] = -1;
@@ -30,7 +30,7 @@ class SearchByDate extends CI_Controller {
 		2.数据库查询结果为空
 		*/
 		if(preg_match($this->dateFormat,$date)){	//判断是否符合日期格式
-			$data['result'] = $this->SearchDateModel->searchOneDate($date);
+			$data['result'] = $this->SearchDateModel->searchOneDateBrief($date);
 			$data['errorFlag'] = 0;
 			if ($data['result'] == null) {
 				$data['errorFlag'] = 2;
@@ -38,7 +38,7 @@ class SearchByDate extends CI_Controller {
 		}else{
 			$data['errorFlag'] = 1;
 		}
-		$this->load->view('selectOneDate',$data);
+		$this->load->view('selectOneDateEp',$data);
 	}
 
 	public function selectDates($dateStart = '',$dateEnd = ''){
--- application/models/SearchDateModel.php
+++ application/models/SearchDateModel.php
@@ -2,24 +2,28 @@
 class SearchDateModel extends CI_Model{
 	function __construct(){
 		parent::__construct();
-		$this->db->query('set names utf8');
+		#$this->db->query('set names utf8');
 	}
-	public function searchOneDate($date){
+	public function searchOneDateBrief($date){
 		$episodeOfOneDay = array();
+		$dateTimeStart = $date.' 00:00:00';
+		$dateTimeEnd = $date.' 23:59:59';
 		$sql = $this->db->query("SELECT * FROM  `episode` 
-			LEFT JOIN  `name` ON  `episode`.`n_id` =  `name`.`n_id` 
-			WHERE  `episode`.`e_onAir` = \"{$date}\"") ->result_array();
+			LEFT JOIN  `shows` ON  `episode`.`s_id` =  `shows`.`s_id` 
+			WHERE  `episode`.`e_time` >= '{$dateTimeStart}' AND `episode`.`e_time` <= '{$dateTimeEnd}' ") ->result_array();
 		foreach ($sql as $rs) {
 			$episodeOfOneDay[] = array(
-				'n_id' => $rs['n_id'],
 				'e_id' => $rs['e_id'],
-				'n_name' => $rs['n_name'],
+				's_id' => $rs['s_id'],
+				'se_id' => $rs['se_id'],
 				'e_name' => $rs['e_name'],
-				'season' => $rs['e_season'],
-				'episode' => $rs['e_episode'],
-				'on_air' => $rs['e_onAir'],//一天就是那一天，不需要提供日期，这里有冗余
-				'photo_link' => $rs['n_photoLink'],
-				'e_description' => $rs['e_description']
+				'e_num' => $rs['e_num'],
+				'e_status' => $rs['e_status'],
+				'e_time' => $rs['e_time'],
+				's_name' => $rs['s_name'],
+				's_sibox_image' => $rs['s_sibox_image'],
+				'area' => $rs['area'],
+				'channel' => $rs['channel']
 				);
 		}
 		unset($sql);
@@ -28,6 +32,8 @@ class SearchDateModel extends CI_Model{
 		else
 			return null;
 	}
+
+
 
 	public function searchDates($dateStart,$dateEnd){
 		//本方法是将指定日期之间所有剧集取出，之后按照日期进行分组返回的方法。日期格式与上面的方法一致
